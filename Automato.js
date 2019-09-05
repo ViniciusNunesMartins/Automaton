4de827ecc73fa3f7050cacf9579c81ea73427f65
Vinicius <nunesmartinsvinicius@gmail.com>
2019-09-04 23:09:42 -0400

    Methods from of elementary classes

--- Automato.js
+++ Automato.js
@@ -5,4 +5,20 @@ export class Automation {
         this.states = [];
         this.initialState = undefined;
     }
+    set state (id, final = false) {
+        this.state.push(new State(id, final));
+    }
+
+    get state (id) {
+        for (let s in this.states) {
+            if (s.id === id) return s;
+        }
+        return undefined;
+    }
+
+    set transition (origin, alphabet, destination, direction = undefined, action = undefined) {
+        if (typeof(origin) === typeof("")) origin = this.state(origin);
+        if (typeof(destination) === typeof("")) destination = this.state(destination);
+        origin.transition(alphabet, destination, direction, action);
+    }
 }
